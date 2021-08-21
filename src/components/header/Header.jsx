0de61fc593928d57fd1dabf41811b3f2dc13de52
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-08-21 09:16:16 +0530

    Add navbar, footer, and header section with video

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./header.css";
+import dog from "./videos/DogVid2.mp4";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="heading-container">
+        <h1>YOUR BEST FRIEND DESERVES</h1>
+        <h1>THE BEST TRAINING</h1>
+      </div>
+      <div className="video-container">
+        <video id="background-video" loop={true} autoPlay={true} muted={true}>
+          <source src={dog} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
